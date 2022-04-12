5217fafa591926c9715b35d6b023de1665425d4f
nataliasue <102632596+nataliasue@users.noreply.github.com>
2022-04-12 20:34:42 -0300

    Add files via upload

--- Desafio 2.py
+++ Desafio 2.py
@@ -1,22 +1,23 @@
-cliente = dict()
-clientes = list()
+cliente = []
+clientes = {}
 
 while True:    
-    cliente = {}
+    cliente = []
     cliente['Nome'] = str(input('Nome do cliente: '))
     cliente['Telefone'] = int(input('Telefone do cliente: '))
     cliente['Email'] = str(input('Email do cliente: '))
     print(cliente)
     
-    clientes.append(cliente)
     proximo = str(input('Próximo cliente?: [S/N] '))
     if proximo == 'S':
         print('Próximo cliente.')
     elif proximo == 'N':
         print('Você saiu do sistema.')
         break
+       
+    clientes.append(cliente)
         
-    
+   
     
     
 
