7276ae545ea318d56267e1d237e50e5346c7f308
nataliasue <102632596+nataliasue@users.noreply.github.com>
2022-04-12 21:26:21 -0300

    Add files via upload

---
+++ Função 1.py
@@ -0,0 +1,20 @@
+def soma (num1, num2):
+    return(num1 + num2)
+def subtrai (num1, num2):
+    return(num1 - num2)
+def divide (num1, num2):
+    return(num1 / num2)
+def multiplica (num1, num2):
+    return(num1 * num2)
+
+num1 = int(input('Informe o primeiro número: '))
+num2 = int(input('Informe o segundo número: '))
+
+r = soma(num1, num2)
+print(r)
+r = subtrai(num1, num2)
+print(r)
+r = divide(num1, num2)
+print(r)
+r = multiplica(num1, num2)
+print(r) 
---
+++ Função 2.py
@@ -0,0 +1,11 @@
+num = 2
+def multiplica (b, h):
+    return(b * h)
+def divide (calcula_triangulo, num):
+    return(calcula_triangulo / num)
+
+
+base = float(input('Informe a base do triângulo: '))
+altura = float(input('Informe a altura do triângulo: '))
+calcula_triangulo = (base * altura) / num
+print(calcula_triangulo)
